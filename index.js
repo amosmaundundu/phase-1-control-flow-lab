678f3205237af63d4adb1e07c77c1f52c74f9bfa
amosmaundundu <amosmaundu5@gmail.com>
2022-08-19 17:16:55 +0300

    index,js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let distance = 199;
+function scuberGreetingForFeet(distance){
+  if(distance<=400){
+   return "This one is on me!";
+  } 
+  if (distance>2500) {
+    return "No can do.";
+  };
+  if (distance>=2000){
+    return "I will gladly take your thirty bucks.";
+  } 
+};
+
+  
+
+function ternaryCheckCity(city){
+return city=='NYC'? 'Ok, sounds good.': 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+function switchOnCharmFromTip(Tip){
+  if (Tip=='generous'){ 
+    return "Thank you so much.";
+  
+} else if (Tip=='not as generous'){
+  return "Thank you."
+} else {
+  return 'Bye.';
+}};
